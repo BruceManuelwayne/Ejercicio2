eb3c9b5ba9ec7c88f7f09ce57d003d4b5bba9a77
Enrique Green <47195508+BruceManuelwayne@users.noreply.github.com>
2021-03-24 21:34:47 -0300

    ejercicio2

---
+++ Ejercicio2/Ejercicio2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Ingrese un texto");
+
+            string texto = Console.ReadLine();
+
+            while (string.IsNullOrEmpty(texto))
+            {
+                Console.WriteLine("Por favor ingrese un texto");
+                texto = Console.ReadLine(); 
+            }
+
+            Console.WriteLine($"El texto ingresado es {texto}");
+            Console.ReadKey(); 
+
+
+
+        }
+    }
+}
